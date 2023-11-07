81139f4e23007e8a0f211ab1d730d215a857f4cb
buithehaiuts <127998752+buithehaiuts@users.noreply.github.com>
2023-11-07 11:51:27 +1100

    Update facts_listings.sql

--- models/warehouse/facts_listings.sql
+++ models/warehouse/facts_listings.sql
@@ -1,4 +1,3 @@
-
 WITH check_dimensions AS
 (
     SELECT
@@ -10,14 +9,18 @@ WITH check_dimensions AS
 )
 
 SELECT
-	lst.*,
-    lga.lga_code AS lga_code,
-    lga.lga_name AS lga_name
+    lst.*,
+    lga_list.lga_code AS LISTING_NEIGHBOURHOOD_LGA_CODE,
+    lga_list.lga_name AS LISTING_NEIGHBOURHOOD_LGA_NAME,
+    COALESCE(lga_host.lga_code, 0) AS HOST_NEIGHBOURHOOD_LGA_CODE,
+    COALESCE(lga_host.lga_name, 'NA') AS HOST_NEIGHBOURHOOD_LGA_NAME
 FROM check_dimensions AS lst
-LEFT JOIN {{ ref('nsw_lga_stg') }} AS lga
-    ON lst.listing_neighbourhood = lga.lga_name
+LEFT JOIN {{ ref('nsw_lga_stg') }} AS lga_list
+    ON lst.listing_neighbourhood = lga_list.lga_name
+LEFT JOIN {{ ref('nsw_lga_stg') }} AS lga_host
+    ON lst.host_neighbourhood = lga_host.lga_name
 LEFT JOIN {{ ref('g01_stg') }} AS g01
-    ON CAST(SUBSTRING(g01.lga_code, 4) AS INTEGER) = CAST(lga.lga_code AS INTEGER)
+    ON CAST(SUBSTRING(g01.lga_code, 4) AS INTEGER) = CAST(lga_list.lga_code AS INTEGER) 
 LEFT JOIN {{ ref('g02_stg') }} AS g02
-    ON CAST(SUBSTRING(g02.lga_code, 4) AS INTEGER) = CAST(lga.lga_code AS INTEGER)
+    ON CAST(SUBSTRING(g02.lga_code, 4) AS INTEGER) = CAST(lga_list.lga_code AS INTEGER)
 WHERE (g01.lga_code IS NOT NULL AND g02.lga_code IS NOT NULL)
